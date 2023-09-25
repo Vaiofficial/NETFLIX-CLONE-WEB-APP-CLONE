07ae51051a91b0b6a69ede57b2b491b7d23077a9
P Vaibhav <pvaibhavofficial@gmail.com>
2023-09-25 19:24:47 +0530

    added profile view section

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const apiPaths = {
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
     fetchTrending: `${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
-    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDM5XFPOPLDhydIWya0eRak538HvWjPXGw`
+    searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDP4oV_mnGrIgzIa56tVjv4lSY_Lnq2sSs`
 }
 
 // 
@@ -17,7 +17,7 @@ const apiPaths = {
 //boots up the app
 
 function init() {
-    
+
     fetchTrendingMovies()
     // fetchAndBuildMovieSection(apiPaths.fetchTrending , 'Trending Now');
     fetchAndBuildAllSections();
@@ -25,31 +25,30 @@ function init() {
 
 
 //trending movies ke sectoion ko show karne ke liye hai ye function.
-function fetchTrendingMovies()
-{
-    fetchAndBuildMovieSection(apiPaths.fetchTrending  ,"Trending Now").then(list=>{
+function fetchTrendingMovies() {
+    fetchAndBuildMovieSection(apiPaths.fetchTrending, "Trending Now").then(list => {
         const randomIndex = parseInt(Math.random() * list.length); //float deta hai so parseInt se int mai convert kar rhe h.
         buildBannerSection(list[randomIndex]);
-    }).catch(err=>{
+    }).catch(err => {
         console.error(err);
     })
 }
 
 //sabse top mai jo banner hai , jo ki trending movies se movies uthakar vha show kar rha hai. uske liye hai ye vala function.
-function buildBannerSection(movie)
-{
+function buildBannerSection(movie) {
     const bannerCont = document.getElementById('banner-section');
     bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
 
     const div = document.createElement('div');
 
-    div.innerHTML= `
+    div.innerHTML = `
     <h2 class="banner__title">${movie.title}</h2> 
     <p class="banner__info">Trending in Movies | Release Date - ${movie.first_air_date}</p>
-    <p class="banner__overview">${movie.overview && movie.overview.length>200 ?movie.overview.slice(0,200).trim()+"...":movie.overview}</p>
+    <p class="banner__overview">${movie.overview && movie.overview.length > 200 ? movie.overview.slice(0, 200).trim() + "..." : movie.overview}</p>
     <div class="action-buttons-cont">
-      <button class="action-button">Play</button>
-      <button class="action-button">More Info</button>
+      <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp;Play</button>
+
+      <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp;More Info</button>
     </div>`
 
     div.className = "banner-content container";
@@ -82,11 +81,11 @@ function fetchAndBuildMovieSection(fetchUrl, categoryName) {
             // console.table(res.results);
             const movies = res.results;
             if (Array.isArray(movies) && movies.length) {
-                buildMoviesSection(movies.slice(0,6),categoryName);
+                buildMoviesSection(movies.slice(0, 6), categoryName);
             }
             return movies;
         })
-        .catch(err=>console.error(err)) //complete movies ki list access kar rhe h us un URL se and ak url ke andar 20 movies hai.console mai check kar lena vaibhav.
+        .catch(err => console.error(err)) //complete movies ki list access kar rhe h us un URL se and ak url ke andar 20 movies hai.console mai check kar lena vaibhav.
 }
 
 function buildMoviesSection(List, categoryName) {
@@ -123,39 +122,57 @@ function buildMoviesSection(List, categoryName) {
 
 
 //movies trailer ko show karne k liye hkar rhe hai.
-function searchMovieTrailer(MovieName , iframeId)
-{
+function searchMovieTrailer(MovieName, iframeId) {
     // console.log( document.getElementById(iframeId) , iframeId);
-    if(!MovieName)return;
+    if (!MovieName) return;
 
     fetch(apiPaths.searchOnYoutube(MovieName))
-    .then(res =>res.json())
-    .then(res=>{
-        // console.log(res.items[0])
-        const bestSearch  = res.items[0];
-        // const youtubeUrl = `https://www.youtube.com/watch?v=${bestSearch.id.videoId}`
-        // console.log(youtubeUrl);
-        const elements = document.getElementById(iframeId);
-        console.log(elements , iframeId)
+        .then(res => res.json())
+        .then(res => {
+            // console.log(res.items[0])
+            const bestSearch = res.items[0];
+            // const youtubeUrl = `https://www.youtube.com/watch?v=${bestSearch.id.videoId}`
+            // console.log(youtubeUrl);
+            const elements = document.getElementById(iframeId);
+            // console.log(elements , iframeId)
 
-        const div = document.createElement('div');
-        div.innerHTML = `<iframe width="245px" height="150px" src="https://www.youtube.com/embed/${bestResult.id.videoId}?autoplay=1&controls=0"></iframe>`;
+            const div = document.createElement('div');
+            div.innerHTML = `<iframe width="245px" height="150px" src="https://www.youtube.com/embed/${bestSearch.id.videoId}?autoplay=1&controls=0"></iframe>`;
 
-        elements.append(div);
-    })
-    .catch(err =>console.error(err))
+            elements.append(div);
+        })
+        .catch(err => console.error(err))
 }
 
+
+
+//PROFILE SECTION VISIT FUNCTION
+const profilePage = document.querySelector('#Profile-Page');
+
+function profileSection() {
+    
+    fetch('profile.html').then(res=>{
+        if(!res.ok){
+            throw new Error("Check Your Network")
+        }
+        return res.text();
+    }).then(content=>{
+        document.body.innerHTML = content;
+    }).catch(err=>{
+        console.error("Problem with Fetch Operation",err);
+    });
+}
+
+profilePage.addEventListener("click" , profileSection);
 
 
 window.addEventListener("load", function () {
     init();
 
-    window.addEventListener("scroll" , function(){
+    window.addEventListener("scroll", function () {
         //header UI Update.
         const header = document.getElementById("header");
-        if(window.scrollY>5)
-        {
+        if (window.scrollY > 5) {
             header.classList.add("black-bg");
         }
         else header.classList.remove('black-bg');
